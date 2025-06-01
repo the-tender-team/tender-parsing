484be334d3c2c71ee5280df017e61e445f3443ee
iannsgirdye <ivan4ik.babi4ik777@yandex.ru>
2025-06-01 14:52:14 +0300

    Update metadata

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -20,7 +20,47 @@ const geistMono = Geist_Mono({
 
 export const metadata: Metadata = {
   title: 'Tender Parsing',
-  description: 'Tender Parsing - ваш надежный помощник в поиске и анализе тендеров.'
+  description: 'Сервис анализа проблемных тендеров на сайте госзакупок',
+  
+  // Иконки для разных платформ
+  icons: {
+    icon: [
+      { url: '/favicon-16x16.png', sizes: '16x16', type: 'image/png' },
+      { url: '/favicon-32x32.png', sizes: '32x32', type: 'image/png' },
+      { url: '/android-chrome-192x192.png', sizes: '192x192', type: 'image/png' },
+      { url: '/android-chrome-512x512.png', sizes: '512x512', type: 'image/png' },
+    ],
+    shortcut: [{ url: '/favicon.ico' }],
+    apple: [{ url: '/apple-touch-icon.png' }],
+  },
+
+  // OpenGraph (для соцсетей: Facebook, LinkedIn, Telegram)
+  openGraph: {
+    title: 'Tender Parsing',
+    description: 'Сервис анализа проблемных тендеров на сайте госзакупок',
+    url: 'https://tender-parsing.ru',
+    siteName: 'Tender Parsing',
+    images: [
+      {
+        url: '/og-image.png',
+        width: 1200,
+        height: 630,
+        alt: 'Сервис анализа проблемных тендеров на сайте госзакупок',
+      },
+    ],
+    locale: 'ru_RU',
+    type: 'website',
+  },
+
+  // Дополнительные мета-теги
+  manifest: '/site.webmanifest',
+  themeColor: '#ffffff',
+  other: {
+    'msapplication-TileColor': '#ffffff',
+    'msapplication-TileImage': '/android-chrome-192x192.png',
+    'yandex-verification': '274cf265ce44da11',
+    'google-site-verification': '',
+  }
 }
 
 export default function RootLayout({ children, }: Readonly<{ children: React.ReactNode; }>) {
